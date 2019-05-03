3fa8cc3e752d74db4a9e2a9e2b853e6596eb3932
pabra <pabra@netzfruech.de>
2019-05-03 08:31:37 +0200

    follow suggestions by shellcheck

--- run_local.sh
+++ run_local.sh
@@ -3,13 +3,13 @@
 BIN_READLINK=$( test -f /usr/local/bin/greadlink && echo "greadlink" || echo "readlink" )
 DIR="$( dirname "$( $BIN_READLINK -f "$0" )" )"
 
-cd ${DIR}
+cd "${DIR}" || exit
 XDG_OPEN=$( which xdg-open )
 
-source ${DIR}/venv/bin/activate
+source "${DIR}/venv/bin/activate"
 
-URL=$( ${DIR}/run.py get_url )
+URL=$( "${DIR}/run.py" get_url )
 
-[ "$XDG_OPEN" ] && sleep 0.5 && $XDG_OPEN $URL >/dev/null 2>&1 &
+[ "$XDG_OPEN" ] && sleep 0.5 && $XDG_OPEN "$URL" >/dev/null 2>&1 &
 
-${DIR}/run.py
+"${DIR}/run.py"
